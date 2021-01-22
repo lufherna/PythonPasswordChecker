2d1da55c3c8accee7ae824dbe92d2e0787235bd2
lufherna <rojoclifford@gmail.com>
2021-01-22 09:13:17 -0500

    Added admin method along with minor changes to the rest of the code.

--- mainCheck.py
+++ mainCheck.py
@@ -1,22 +1,45 @@
 # regex library import
 import re
 
-userRole = input("What role do you have? Type in 'admin' or 'normal' to continue ")
-userPassword = input("Enter password! Must contain at least 1 letter, 1 number and be at least 8 characters long: ")
+userRoleInput = input("What role do you have? Type in 'admin' or 'normal' to continue ").lower()
+userRole = userRoleInput.replace(" ", "")
+userPassword = input("Enter password! Must contain at least 1 letter,\n 1 number and be at \n least 8 characters long: ").lower()
+
+if userRole != 'admin':
+    print(userRole)
 
 
 # iteration 1
 class PassCheck1:
-    # def __init__(self, password):
-    #   self.password = password
+    def __init__(self, role, password):
+        self.userRole = role
+        self.password = password
 
-    def regexSearch(password):
+    def normalUser(password):
         minimumChar = 8
 
         # added this in case the user adds spaces within their passwords
-        noSpacesPassword = password.replace(" ", "").lower()
+        noSpacesPassword = password.replace(" ", "")
 
-        # by making all passwords lowercase makes it a bit easier to work with regex
+        #  using re.search in order to make sure the given password has the necessary characters
+        regexPattern = re.search("[a-z][0-9]", noSpacesPassword)
+
+        if len(password) < minimumChar:
+            print("Password is too short!")
+        elif password.isdigit():
+            print("Your password needs at least one letter. C'mon!")
+        elif not regexPattern:
+            print("Password is missing a number!")
+        else:
+            print("Password looks good!")
+
+    def adminUser(password):
+        minimumChar = 13
+
+        # added this in case the user adds spaces within their passwords
+        noSpacesPassword = password.replace(" ", "")
+
+        #  using re.search in order to make sure the given password has the necessary characters
         regexPattern = re.search("[a-z][0-9]", noSpacesPassword)
 
         if len(password) < minimumChar:
@@ -29,4 +52,9 @@ class PassCheck1:
             print("Password looks good!")
 
 
-firstUserCheck = PassCheck1.regexSearch(userPassword)
+
+
+
+
+
+firstUserCheck = PassCheck1(userRole,userPassword)
