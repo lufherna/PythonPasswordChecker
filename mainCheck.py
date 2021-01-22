8761e580ae05d30db9fb1fe50c17221300d7a121
lufherna <rojoclifford@gmail.com>
2021-01-22 01:21:30 -0500

    changed the way regex checks the password and made sure the passwords are lowercase

--- mainCheck.py
+++ mainCheck.py
@@ -3,21 +3,24 @@ import re
 
 userPassword = input("Enter password! Must contain at least 1 letter, 1 number and be at least 8 characters long: ")
 
+
 # iteration 1
 class PassCheck1:
-  # def __init__(self, password):
-  #   self.password = password
+    # def __init__(self, password):
+    #   self.password = password
 
-  minimumChar = 8
+    def regexSearch(password):
+        minimumChar = 8
 
-  def regexSearch(password):
+        # added this in case the user adds spaces within their passwords
+        noSpacesPassword = password.replace(" ", "")
 
-    noSpacesPassword = password.replace(" ", "")
-    regexPattern = re.search("[A-Z][a-z][0-9]", noSpacesPassword)
-    if regexPattern and len(password) >= 8:
-      print("Password looks good!")
-    else:
-      print("Password is missing required characters")
+        # by making all passwords lowercase makes it a bit easier to work with regex
+        regexPattern = re.search("[a-z][0-9]", noSpacesPassword.lower())
+        if regexPattern and len(password) >= minimumChar:
+            print("Password looks good!")
+        else:
+            print("Password is missing required characters")
 
 
 firstUserCheck = PassCheck1.regexSearch(userPassword)
