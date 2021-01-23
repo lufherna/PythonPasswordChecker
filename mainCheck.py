2f37bfbbdad437051669c780e3afeac58c126ea7
lufherna <rojoclifford@gmail.com>
2021-01-22 20:21:34 -0500

    Was able to change the regex around to check if a string has a number and used that within the elif statement. Moving on to iteration 4

--- mainCheck.py
+++ mainCheck.py
@@ -7,6 +7,7 @@ userPassword = None
 while True:
   userRole = input("What role do you have? Type 'admin' or 'normal' to continue: ").lower().replace(" ", "")
 
+  # compares the userRole to strings in the set
   if userRole in ('admin', 'normal'):
     print("User role is accepted")
     userPassword = input("Enter password! Must contain at least 1 letter, 1 number and be at least 8 characters long: ").lower().replace(" ", "")
@@ -26,7 +27,7 @@ class PassCheck1:
         minimumChar = 8
 
         #  using re.search in order to make sure the given password has the necessary characters
-        regexPattern = re.search("[a-z][0-9]", password)
+        regexPattern = re.search("[a-z0-9]", password)
 
         if len(password) < minimumChar:
             print("Password is too short! Needs to be at least 8 characters long.")
@@ -41,25 +42,30 @@ class PassCheck1:
         minimumChar = 13
 
         #  using re.search in order to make sure the given password has the necessary characters
-        regexPattern = re.search("[a-z][0-9]", password)
+        regexPattern = re.search("[a-z0-9][@!#$%^&*]", password)
+        # used regex search to grab the specific info regarding numbers inside the password
+        hasNumber = re.search(r'\d', password)
 
+        # checks minimum length
         if len(password) < minimumChar:
-            print("Password is too short!")
+            print("Password is too short! Needs to be at least 13 characters long")
+        # checks if password is only numbers
         elif password.isdigit():
-            print("Your password needs at least one letter. C'mon!")
+            print("Your password needs at least one letter!")
+        elif not hasNumber:
+            print("Your password needs at least one number!")
         elif not regexPattern:
-            print("Password is missing a number!")
+            print("Password is missing a special character!")
         else:
             print("Password looks good!")
 
 # conditional statement that'll call a method depending on the user role
 if userRole == 'admin':
     adminUser = PassCheck1.adminUser(userPassword)
-    print(adminUser)
 
-elif userRole == 'normal':
+else:
     normalUser = PassCheck1.normalUser(userPassword)
-    print(normalUser)
+
 
 
 
